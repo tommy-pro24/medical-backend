80dfc54b19df110856e7731be57f109c36c70c94
tommy-pro24 <devtommy1031@gmail.com>
2025-05-19 00:07:29 +0800

    add updateStock function

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from 'express';
+import { ErrorResponse } from '../utils/errorResponse';
+import { Product } from '../models/product';
+import { User } from '../models/User';
+
+export const updateStock = async (req: Request, res: Response): Promise<any> => {
+
+    try {
+
+        const user = await User.findById(req.user._id);
+
+        if (!user) {
+            throw new ErrorResponse('User not found', 404);
+        }
+
+        if (user.role !== 'admin') {
+            throw new ErrorResponse('Unauthorized user', 403);
+        }
+
+        const { id, newStock } = req.body;
+
+        await Product.findByIdAndUpdate(
+            id,
+            { stockNumber: newStock },
+            { new: true }
+        );
+
+        res.status(200);
+
+    } catch (error) {
+
+        if (error instanceof ErrorResponse) {
+            res.status(error.statusCode).json({ message: error.message });
+        } else {
+            res.status(500).json({ message: 'Server error' });
+        }
+
+    }
+
+}
