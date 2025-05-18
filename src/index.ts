4fb02f3bb4aa5c6d09fd034b593dbf870d36d296
tommy-pro24 <devtommy1031@gmail.com>
2025-05-19 00:05:05 +0800

    remove cors data from index.ts

--- src/index.ts
+++ src/index.ts
@@ -14,12 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(helmet());
-app.use(cors({
-    origin: 'http://localhost:3000', // Your frontend URL
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
